52bc3174b61e59e655244bef9c9217ac72c3930d
brunofreiberger <brunohfreiberger@gmail.com>
2015-12-07 00:24:11 -0200

    adicionado as funções de excluir e alterar

--- produtos/_attachments/crud.js
+++ produtos/_attachments/crud.js
@@ -1,5 +1,6 @@
 /** 
  * Bruno Henrique Freiberger
+ * Material de consulta: http://www.ibm.com/developerworks/opensource/tutorials/os-couchapp/#list6
  * http://127.0.0.1:5984/produtos/_design/produtos/_view/viewprodutos
  * couchapp push http://127.0.0.1:5984/produtos
  * couchapp generate view viewprodutos
@@ -10,94 +11,79 @@
 db = $.couch.db("produtos");
 
 //Function que salva os produtos na base
-function salvar(event){
-	var form = $(event.target).parents('form#btn-add');
-	//TODO remover
-	console.log('Verificando id do formulário: ' + form.id);
-	
-	var nome = $("#inp_nome").val();
-	var valor = $("#inp_valor").val();
-	var fornecedor = $("#inp_fornecedor").val();
-	var descricao = $("#inp_descricao").val();
-	
-	if(validaCampos(nome, valor, fornecedor, descricao)) {
-		var produto = new Produto(nome, valor, fornecedor, descricao);
-		var doc = produto;
-		db.saveDoc(doc, {
-			success: function() { 
-				$("#messages").removeClass("alert-danger");
-				$("#messages").addClass("alert-info").css("display", "block").text("Produto cadastrado com sucesso!").hide().fadeIn(500);
-			},
-		});
-	} 
+function salvar(edicao, form, doc){
+	db.saveDoc(doc, {
+		success: function() { 
+			$("#messages").removeClass("alert-danger");
+			$("#messages").addClass("alert-info").css("display", "block").text(!edicao ? "Produto cadastrado com sucesso!" : "Produto alterado com sucesso!").hide().fadeIn(500);
+			if(edicao)
+				consultar();
+		},
+	});
 }
 
-//Trigger para invocar a função de salvar
-$("input#btn-add").live('click', function(event){
-	salvar(event);
-});
-
-//Trigger para invocar a função de consultar
-$("input#btn-consultar").live('click', function(event){
-	consultar();
-});
-
-//Conversor de obj js em json
-function converteProdutoJson(produto){
-	//null, "\t" -> indenta para apresentar no console
-	var produtoJSON = JSON.stringify(produto, null, "\t");
-	console.log(produtoJSON);
-	return produtoJSON;
-}
-
-//Validação Simples dos campos
-function validaCampos(nome, valor, fornecedor, descricao){
-	if(!nome || !valor || !fornecedor || !descricao) {
-		$("#messages").removeClass("alert-info");
-		$("#messages").addClass("alert-danger").css("display", "block").text("Preencha todos os campos!").hide().fadeIn(500);
-		return false;
+//Função que excluir os registros
+function excluir(){
+	var id = $('#id-registro-selecionado').val();
+	if(id != null && id != ''){
+		db.openDoc(id, {
+			success: function(doc) {
+				db.removeDoc(doc, { 
+					success: function() {
+						$("#messages").removeClass("alert-danger");
+						$("#messages").addClass("alert-info").css("display", "block").text("Produto excluido com sucesso!").hide().fadeIn(500);
+						consultar();
+					}
+				});
+			}
+		});	
 	}
-	return true;
 }
 
-//Construtor de produto
-function Produto(nome, valor, fornecedor, descricao){
-	this.nome = nome;
-	this.valor = valor;
-	this.fornecedor = fornecedor;
-	this.descricao = descricao;
+//Função de cadastrar
+function cadastrar(event) {
+	var form = $(event.target).parents('form#btn-add');
+	if(createProduto() != null) {
+		var doc = createProduto();
+		salvar(false, form, doc);
+	}
 }
 
-function createRow(produto, id){
-	
-	var columns = [];
-	var row = $("<tr>");
-	var col_nome = $('<td>').html(produto.nome);
-	var col_valor = $('<td>').html(produto.valor);
-	var col_fornecedor = $('<td>').html(produto.fornecedor);
-	var col_descricao = $('<td> <button type="button" class="btn btn-primary btn-show-descricao" id="'+id+'" data-toggle="modal" data-target=".fade">Visualizar</button></div>');
-	
-	columns.push(col_nome);
-	columns.push(col_valor);
-	columns.push(col_fornecedor);
-	columns.push(col_descricao);	
-	
-	$.each(columns, function(index, value){
-		row.append(value);
-	})
-	
-	return row;
+//Função de edição de registros
+function editar(){
+	var form = $(event.target).parents('form#btn-salvar-edicao');
+	var id = $('#id-registro-selecionado').val();
+	if(id != null && id != ''){
+		db.openDoc(id, {
+			success: function(doc) {
+				var produto = createProdutoEdicao(doc);
+				if(produto != null) {
+					doc = produto;
+					salvar(true, form, doc);
+				}
+			}
+		});
+	}
+
 }
 
-//Trigger para invocar a função de consultar
-$("button.btn-show-descricao").live('click', function(event){
-	var id = event.target.id;
+//Carrega os campos da tela de edição
+function loadEditar(id){
+	$('#id-registro-selecionado').val(id);
 	db.openDoc(id, { success: function(doc) { 
-		$('.dialog-nome-produto').text('Produto: '+ doc.nome);
-		$('#dialog-descricao-produto').html('Descrição: '+doc.descricao);	
+		$('#inp_nome_dialog').val(doc.nome);
+		$('#inp_valor_dialog').val(doc.valor);	
+		$('#inp_fornecedor_dialog').val(doc.fornecedor);
+		$('#inp_descricao_dialog').val(doc.descricao);
 	}});
-});
+}
 
+//Carrega a confirmação da exclusão
+function loadExcluir(id){
+	$('#id-registro-selecionado').val(id);
+}
+
+//Função de consulta
 function consultar(){
 	
 	var table = $('#table-results');
@@ -130,3 +116,137 @@ function consultar(){
 	table.append(body_col);
 
 }
+
+//Cria produtos apartir do formulário da tela de edição
+function createProdutoEdicao(doc){
+	var nome = $("#inp_nome_dialog").val();
+	var valor = $("#inp_valor_dialog").val();
+	var fornecedor = $("#inp_fornecedor_dialog").val();
+	var descricao = $("#inp_descricao_dialog").val();
+	return buildProduto(doc, nome, valor, fornecedor, descricao)
+}
+
+//Cria produto apartir dos campos da tela
+function createProduto(){
+	var nome = $("#inp_nome").val();
+	var valor = $("#inp_valor").val();
+	var fornecedor = $("#inp_fornecedor").val();
+	var descricao = $("#inp_descricao").val();
+	return buildProduto(null, nome, valor, fornecedor, descricao)
+}
+
+//Cria produto apartir do formulário
+function buildProduto(doc, nome, valor, fornecedor, descricao){
+	var produto = null;
+	if(validaCampos(nome, valor, fornecedor, descricao)) {
+		if(doc!= null) {
+			doc.nome = nome;
+			doc.valor = valor;
+			doc.fornecedor = fornecedor;
+			doc.descricao = descricao;
+			produto = doc;
+		} else {
+			produto = new Produto(nome, valor, fornecedor, descricao);
+		}
+	} else {
+		produto = null;
+	}
+	return produto;	
+}
+
+//Conversor de obj js em json
+function converteProdutoJson(produto){
+	//null, "\t" -> indenta para apresentar no console
+	var produtoJSON = JSON.stringify(produto, null, "\t");
+	console.log(produtoJSON);
+	return produtoJSON;
+}
+
+//Validação Simples dos campos
+function validaCampos(nome, valor, fornecedor, descricao){
+	if(!nome || !valor || !fornecedor || !descricao) {
+		$("#messages").removeClass("alert-info");
+		$("#messages").addClass("alert-danger").css("display", "block").text("Preencha todos os campos!").hide().fadeIn(500);
+		return false;
+	}
+	return true;
+}
+
+//Construtor de produto
+function Produto(nome, valor, fornecedor, descricao){
+	this.nome = nome;
+	this.valor = valor;
+	this.fornecedor = fornecedor;
+	this.descricao = descricao;
+}
+
+//Função que cria a linha da tabela de consulta
+function createRow(produto, id){
+	
+	var columns = [];
+	var row = $("<tr>");
+	var col_nome = $('<td>').html(produto.nome);
+	var col_valor = $('<td>').html(produto.valor);
+	var col_fornecedor = $('<td>').html(produto.fornecedor);
+	var col_descricao = $('<td> <button type="button" class="btn btn-primary btn-show-descricao" id="'+id+'" data-toggle="modal" data-target="#dialog-descricao">Visualizar</button></td>');
+	var col_editar = $('<td> <button type="button" class="btn btn-success btn-editar" style="width: 100%" id="'+id+'" data-toggle="modal" data-target="#dialog-editar">Editar</button></td>');
+	var col_excluir = $('<td> <button type="button" class="btn btn-danger btn-excluir" style="width: 100%" id="'+id+'" data-toggle="modal" data-target="#dialog-confirm-exclusao">Excluir</button></td>');
+	
+	columns.push(col_nome);
+	columns.push(col_valor);
+	columns.push(col_fornecedor);
+	columns.push(col_descricao);
+	columns.push(col_editar);
+	columns.push(col_excluir);
+		
+	
+	$.each(columns, function(index, value){
+		row.append(value);
+	})
+	
+	return row;
+}
+
+//'Triggers' dos botões
+
+//Trigger para invocar a função de salvar
+$("input#btn-add").live('click', function(event){
+	cadastrar(event);
+});
+
+//Trigger para invocar a função de consultar
+$("input#btn-consultar").live('click', function(event){
+	consultar();
+});
+
+//Trigger para invocar a carregar os campos da edição
+$("button.btn-editar").live('click', function(event){
+	var id = event.target.id;
+	loadEditar(id);
+});
+
+//Trigger para invocar a função para salvar edição
+$("button#btn-salvar-edicao").live('click', function(event){
+	var id = event.target.id;
+	editar(id);
+});
+
+//Trigger para invocar a função de exclusao
+$("button.btn-excluir").live('click', function(event){
+	var id = event.target.id;
+	loadExcluir(id);
+});
+
+//Trigger para invocar a função de exclusão
+$("button#btn-confirma-exclusao").live('click', function(event){
+	excluir();
+});
+
+//Trigger para invocar a função de carregar descrição
+$("button.btn-show-descricao").live('click', function(event){
+	var id = event.target.id;
+	db.openDoc(id, { success: function(doc) { 
+		$('.dialog-nome-produto').text('Produto: '+ doc.nome);
+		$('#dialog-produto').html('Descrição: '+doc.descricao);	
+	}});
+});
